d01240ab4c9371a2de737ef299bec5bf66c6b500
Shraddha Shrivastav <shraddha.shrivastav@alfalaval.com>
2023-03-11 15:57:33 +0530

    exception exceptionHandling01 updates

--- exceptionHandling01.py
+++ exceptionHandling01.py
@@ -1,7 +1,19 @@
 
 try:
-    print(1/0)
+   open("file.tex")
 except NameError:
     print("NameError exception is thrown because the variable is not defined")
 except ArithmeticError:
-    print("ArithmeticError exception is thrown")
+    print("ArithmeticError exception is thrown.")
+except FileNotFoundError:
+    print("FileNotFoundError exception is thrown.")
+
+
+
+
+while True:
+    try:
+        input = int(input("please enter a number : "))
+        break
+    except:
+        print("only numbers are allowed as input , please try again!")
